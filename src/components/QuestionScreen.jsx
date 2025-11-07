29c0e4462aeeaab2c0bdd71992499fd7c2161a50
ecomshriram <ecom.shriram@gmail.com>
2025-11-07 16:02:09 +0530

    Update QuestionScreen.jsx

--- src/components/QuestionScreen.jsx
+++ src/components/QuestionScreen.jsx
@@ -8,7 +8,7 @@ export default function QuestionScreen({ question, onYes, isFirst }) {
 
     const handleNo = async () => {
         if (isFirst) {
-            // First question - "Do you like surprises?"
+            // First question - "Pillu! Do you like surprises?"
             await Swal.fire({
                 title: "But this one is special!",
                 text: "You need to open it... please?",
